a1b662d744876358d34759285d60139425cd8ace
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-14 14:57:21 +0100

    5.2 search box in navbar, styled new/edit forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  def self.search(search_term)
+    if group = :development
+      Product.where("name LIKE ?", "%#{search_term}%")
+    elsif group = :production
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
+  end
 end
