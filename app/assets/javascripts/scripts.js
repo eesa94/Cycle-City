e3c4318adca81016839f32b295f4ba96474dee87
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-17 19:42:59 +0100

    5.3.1 testing sendgrid

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,16 +1,17 @@
 // document ready functions
 $(document).ready(function(){
-  // scrollspy
-  var $root = $('html, body');
-  $('.navbar-nav a, #about a').click(function() {
-    var href = $.attr(this, 'href');
-    if (href != undefined && href != '#') {
-      $root.animate({
-        scrollTop: $(href).offset().top
-      }, 500, function () {
-        window.location.hash = href;
-      });
-    }
-    return false;
+  // animation
+  $('#q').click(function() {
+    $('#search-btn').addClass('animated pulse').one('animationend', function() {
+      $(this).removeClass('animated pulse');
+    });
   });
+  // $('.animated').removeClass('hidden').addClass('visible');
+
+
 });
+
+// $(window).on('load', function() {
+//   $('#body-content').css('opacity', '1.0');
+//     console.log('window load occurred');
+// });
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
+  post 'simple_pages/thank_you'
   get 'simple_pages/contact'
   get 'simple_pages/about'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
