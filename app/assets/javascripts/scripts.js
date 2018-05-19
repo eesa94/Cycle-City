05760da92462ea64438e6c4c9dda22fceae3451b
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-19 13:29:25 +0100

    5.4 exercise submission - generated user scaffold and login/logout capabilities

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,17 +1,1 @@
-// document ready functions
-$(document).ready(function(){
-  // animation
-  $('#q').click(function() {
-    $('#search-btn').addClass('animated pulse').one('animationend', function() {
-      $(this).removeClass('animated pulse');
-    });
-  });
-  // $('.animated').removeClass('hidden').addClass('visible');
 
-
-});
-
-// $(window).on('load', function() {
-//   $('#body-content').css('opacity', '1.0');
-//     console.log('window load occurred');
-// });
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :products
   post 'simple_pages/thank_you'
