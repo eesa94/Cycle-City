a39d973ff67974bc67a22334082bda962355a89d
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-22 15:22:00 +0100

    5.7 created comment model and styled product show view

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,18 @@
 class Product < ApplicationRecord
+  has_many :comments
   def self.search(search_term)
     if Rails.env.production?
       Product.where("name ilike ?", "%#{search_term}%")
     else
       Product.where("name LIKE ?", "%#{search_term}%")
     end
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
