e17f2e2b6c8c7a28ab4fa357bea6d93201f2a0ab
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-29 15:51:50 +0100

    5.10 added admin functionality and edited cancan abilities

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,13 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
+      can :read, :all
+    end
   end
+  # for this to work, you must add load_and_authorize_resource at the top of the relevant controller
 
 end
