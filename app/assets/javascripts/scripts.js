d607ffb01bef099f03bbe9b6226bfdecd78a1b1e
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-05-31 20:21:29 +0100

    5.13 styled the sidenav

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,26 +1,22 @@
 $(document).on('turbolinks:load', function() {
   $('#body-content').css('opacity', '1.0');
 
-  $('#q').click(function() {
-    $('#search-btn').addClass('animated pulse').one('animationend', function() {
-      $(this).removeClass('animated pulse');
-    });
-  });
-
   $('.alert').addClass('animated alert-animation lightSpeedOut');
 
   $('#sidenav-open-btn').click(function() {
-    // $('.primary-navbar').css('display', 'none');
-    $('.sidenav').css('width', '250px');
-    $('.main-container').css('margin-left', '250px');
+    $('.sidenav').css('width', '300px');
+    $('.main-container, .primary-navbar').css('margin-left', '300px');
     $('body').css('background-color', 'rgba(0,0,0,0.4)');
+    $('.primary-navbar, footer').css('background-color', '#999999');
+    $(this).css('opacity', '0.1');
   });
 
   $('#sidenav-close-btn').click(function() {
-    // $('.primary-navbar').css('display', 'flex');
     $('.sidenav').css('width', '0');
-    $('.main-container').css('margin-left', '0');
+    $('.main-container, .primary-navbar').css('margin-left', '0');
     $('body').css('background-color', 'white');
+    $('.primary-navbar, footer').css('background-color', '#fab297');
+    $('#sidenav-open-btn').css('opacity', '1.0')
   });
 
 
