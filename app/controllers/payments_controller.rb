902aa2a7738c9b7c4817c631fabe70e23e321923
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-08-02 16:39:48 +0100

    6.6.1 created confirmation page after payment, attempted to fix order page error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -28,7 +28,6 @@ class PaymentsController < ApplicationController
           flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
     end
 
-    redirect_to product_path(@product)
   end
 
 end
