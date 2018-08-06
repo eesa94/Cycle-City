d5b59c9ccfc7c766cfcd460a2d4c053038abc7da
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-08-06 12:19:05 +0100

    6.9 javascript on rails - added zoom plugin

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -5,15 +5,11 @@ $(document).on('turbolinks:load', function() {
 
   $('#sidenav-open-btn').click(function() {
     $('.sidenav').css('width', '300px');
-    $('body').css('background-color', 'rgba(0,0,0,0.4)');
-    $('.primary-navbar, footer').css('background-color', '#999999');
     $(this).css('opacity', '0.1');
   });
 
   $('#sidenav-close-btn').click(function() {
     $('.sidenav').css('width', '0');
-    $('body').css('background-color', 'white');
-    $('.primary-navbar, footer').css('background-color', '#fab297');
     $('#sidenav-open-btn').css('opacity', '1.0')
   });
 
