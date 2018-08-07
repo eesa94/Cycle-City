9d7b94a5916cbc77abf0f3cfe74b91d2f88ef3e1
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-08-07 17:26:03 +0100

    6.13 real-time updates with actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://cyclecity.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://cyclecity.herokuapp.com', 'http://cyclecity.herokuapp.com']
 end
