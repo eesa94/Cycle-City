7f27d189e8fdbe9403711505bb7b0e7a61fffe30
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-08-09 11:48:57 +0100

    6.22 fixed nomethoderror in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_product, except: [:index, :new, :create]
+  before_action :set_user, except: [:index, :new, :create]
   before_action :authenticate_user!
   load_and_authorize_resource
 
