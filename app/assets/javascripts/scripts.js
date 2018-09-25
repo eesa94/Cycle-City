7800102f0efb05b4ed863c35eac5f4ee205c2ea6
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-09-25 16:42:52 +0100

    8.3 nav overlays

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -14,6 +14,7 @@ $(document).on('turbolinks:load', function() {
   });
 
 
+  // navbar expand/collapse
   let state = 0;
 
   $('.hamburger').click(function() {
@@ -45,10 +46,53 @@ $(document).on('turbolinks:load', function() {
     }
   });
 
-  $('.nav-item-overlay').click(function() {
+  // nav item click
+  $('.nav-item').click(function() {
     $('.nav-container').css('top', '-100%').css('transition-delay', '0.5s');
     $('.body-content').addClass('fadeIn').removeClass('fadeOut');
   });
+
+
+  // nav item hover
+  $('#nav-item-1').hover(
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.3)').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+    $(this).find('.overlay-header').css('top', '-10%');
+    $(this).find('.hover-fadeUp').css('top', '0%').css('opacity', '1.0');
+  },
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.0)').css('border', '0').css('height', '100%').css('width', '100%');
+    $(this).find('.overlay-header').css('top', '0%');
+    $(this).find('.hover-fadeUp').css('top', '15%').css('opacity', '0');
+    }
+  );
+
+  $('#nav-item-2').hover(
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.3)').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+    $(this).find('.overlay-header').css('top', '-10%');
+    $(this).find('.hover-fadeUp').css('top', '0%').css('opacity', '1.0');
+  },
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.0)').css('border', '0').css('height', '100%').css('width', '100%');
+    $(this).find('.overlay-header').css('top', '0%');
+    $(this).find('.hover-fadeUp').css('top', '15%').css('opacity', '0');
+    }
+  );
+
+  $('#nav-item-3').hover(
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.3)').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+    $(this).find('.overlay-header').css('top', '-10%');
+    $(this).find('.hover-fadeUp').css('top', '0%').css('opacity', '1.0');
+  },
+    function() {
+    $(this).find('.nav-item-overlay').css('background-color', 'rgba(0, 0, 0, 0.0)').css('border', '0').css('height', '100%').css('width', '100%');
+    $(this).find('.overlay-header').css('top', '0%');
+    $(this).find('.hover-fadeUp').css('top', '15%').css('opacity', '0');
+    }
+  );
+
 
 
 });
