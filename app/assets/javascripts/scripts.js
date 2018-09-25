ac1932054fd47c31fc19f518d6867fc47357a898
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-09-25 11:06:24 +0100

    8.1 position fixed - nav burger and menu

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -17,23 +17,27 @@ $(document).on('turbolinks:load', function() {
   let state = 0;
 
   $('.hamburger').click(function() {
+
     if (state === 0) {
       $(this).toggleClass('is-active');
 
       $('.nav-container').css('top', '0').css('transition-delay', '0.1s');
 
       $('.nav-row').addClass('fadeInDown').removeClass('fadeOutUp');
+
       $('.body-content').addClass('fadeOut').removeClass('fadeIn');
 
       state = 1;
 
     } else
     if (state === 1) {
+
       $(this).toggleClass('is-active');
 
       $('.nav-container').css('top', '-100%').css('transition-delay', '0.4s');
 
       $('.nav-row').addClass('fadeOutUp').removeClass('fadeInDown');
+
       $('.body-content').addClass('fadeIn').removeClass('fadeOut');
 
       state = 0;
