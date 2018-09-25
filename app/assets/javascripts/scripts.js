a39c8f2409b241f972f575ab6f54c2493fcb4e8c
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-09-25 10:03:04 +0100

    7.9 hamburger; delete pre-nav layer

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -16,25 +16,32 @@ $(document).on('turbolinks:load', function() {
 
   let state = 0;
 
-  $('.nav-open').click(function() {
+  $('.hamburger').click(function() {
     if (state === 0) {
-      $('.pre-nav-layer').css('top', '0').css('transition-delay', '0s');
+      $(this).toggleClass('is-active');
+
       $('.nav-container').css('top', '0').css('transition-delay', '0.1s');
+
       $('.nav-row').addClass('fadeInDown').removeClass('fadeOutUp');
       $('.body-content').addClass('fadeOut').removeClass('fadeIn');
+
       state = 1;
+
     } else
     if (state === 1) {
-      $('.pre-nav-layer').css('top', '-100%').css('transition-delay', '0.5s');
+      $(this).toggleClass('is-active');
+
       $('.nav-container').css('top', '-100%').css('transition-delay', '0.4s');
+
       $('.nav-row').addClass('fadeOutUp').removeClass('fadeInDown');
       $('.body-content').addClass('fadeIn').removeClass('fadeOut');
+
       state = 0;
+
     }
   });
 
   $('.nav-item-overlay').click(function() {
-    $('.pre-nav-layer').css('top', '-100%').css('transition-delay', '0.5s');
     $('.nav-container').css('top', '-100%').css('transition-delay', '0.5s');
     $('.body-content').addClass('fadeIn').removeClass('fadeOut');
   });
