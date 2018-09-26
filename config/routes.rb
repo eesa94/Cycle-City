aa9b08943d97829fb72ebcc88970f8e5ccf9bda1
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-09-26 12:25:07 +0100

    8.8 edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   post 'simple_pages/thank_you'
-  get 'simple_pages/contact'
-  get 'simple_pages/about'
-  get 'simple_pages/index'
+  get '/contact', to: 'simple_pages#contact'
+  get '/about', to: 'simple_pages#about'
+  get '/', to: 'simple_pages#index'
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
