e7b7051cfb6def13636e9ddf897ddab81bcc1114
Eesa Patel <30774374+eesa94@users.noreply.github.com>
2018-10-01 20:59:59 +0100

    11.8 media queries for home, about, and navbar

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -58,44 +58,46 @@ $(document).on('turbolinks:load', function() {
 
 
   // nav item hover
-  $('#nav-item-1').hover(
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
-    $(this).find('.overlay-header').css('top', '25%');
-    $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
-  },
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
-    $(this).find('.overlay-header').css('top', '40%');
-    $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
-    }
-  );
+  if ($(window).width() > 991) {
+    $('#nav-item-1').hover(
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+      $(this).find('.overlay-header').css('top', '25%');
+      $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
+    },
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
+      $(this).find('.overlay-header').css('top', '40%');
+      $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
+      }
+    );
 
-  $('#nav-item-2').hover(
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
-    $(this).find('.overlay-header').css('top', '25%');
-    $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
-  },
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
-    $(this).find('.overlay-header').css('top', '40%');
-    $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
-    }
-  );
+    $('#nav-item-2').hover(
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+      $(this).find('.overlay-header').css('top', '25%');
+      $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
+    },
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
+      $(this).find('.overlay-header').css('top', '40%');
+      $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
+      }
+    );
 
-  $('#nav-item-3').hover(
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
-    $(this).find('.overlay-header').css('top', '25%');
-    $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
-  },
-    function() {
-    $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
-    $(this).find('.overlay-header').css('top', '40%');
-    $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
-    }
-  );
+    $('#nav-item-3').hover(
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'linear-gradient(to right, rgba(77, 179, 237, 0.5), rgba(109, 177, 255, 0.5), rgba(153, 172, 255, 0.5), rgba(202, 163, 255, 0.5), rgba(248, 150, 253, 0.5))').css('border', '3px solid white').css('height', '80%').css('width', '80%');
+      $(this).find('.overlay-header').css('top', '25%');
+      $(this).find('.hover-fadeUp').css('top', '40%').css('opacity', '1.0');
+    },
+      function() {
+      $(this).find('.nav-item-overlay').css('background-image', 'none').css('border', '0').css('height', '100%').css('width', '100%');
+      $(this).find('.overlay-header').css('top', '40%');
+      $(this).find('.hover-fadeUp').css('top', '70%').css('opacity', '0');
+      }
+    );
+  };
 
   $('.action-background').hover(
     function() {
